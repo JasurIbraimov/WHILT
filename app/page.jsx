bd706bcfadea5084181326335a39d97ec9e96ba3
Jasur <ibraimov0409@mail.ru>
2023-08-02 18:58:22 +0600

    First page

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
-import Feed from "@components/Feed"
+import Feed from "@components/Feed";
 
 const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Share your 
-        <br className="max-md:hidden"/>
-        <span className="purple_gradient text-center"> knowledge!</span>
-      </h1>
-      <p className="desc text-center">
-        <span className="purple_gradient font-bold">WHILT</span> is an open-source tool where you can share your daily knowledge and discover other programmers.
-      </p>
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Share your
+                <br className="max-md:hidden" />
+                <span className="purple_gradient text-center"> knowledge!</span>
+            </h1>
+            <p className="desc text-center">
+                <span className="purple_gradient font-bold">WHILT</span> is an
+                open-source tool where you can share your daily knowledge and
+                discover others.
+            </p>
 
-      <Feed />
-    </section>
-  )
-}
+            <Feed />
+        </section>
+    );
+};
 
-export default Home
+export default Home;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,26 +6,105 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const [isUserLoggedIn, setUserLoggedIn] = useState(true)
+    const [isUserLoggedIn, setUserLoggedIn] = useState(true);
+    const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const providers = await getProviders();
+            setProviders(providers);
+        };
+        fetchProviders();
+    }, []);
+
     return (
         <nav className="menu">
             <Link className="gap-2" href="/">
-                <Image src="/assets/images/logo-light.svg" width={30} height={30} />
+                <Image
+                    alt="WHILT Logo"
+                    src="/assets/images/logo-light.svg"
+                    width={30}
+                    height={30}
+                />
             </Link>
 
             {/* Desktop nav */}
             <div className="sm:flex hidden">
-              {isUserLoggedIn ? (
-                <div className="flex gap-3 md:gap-5">
-                  <Link href={"/create"} className="light_btn">
-                    Create Post
-                  </Link>
-                </div>
-              ) : (
-                <>
-                
-                </>
-              )}
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href={"/create"} className="btn">
+                            Create Post
+                        </Link>
+                        <button className="btn_reversed">Sign out</button>
+                        <Link href={"/profile"} className="rounded-full">
+                            <Image
+                                alt="User Profile"
+                                width={30}
+                                height={30}
+                                src={"/assets/images/logo.svg"}
+                            />
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+
+            {/* Mobile nav */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <button
+                            onClick={() => setToggleDropdown((prev) => !prev)}
+                        >
+                            <Image
+                                alt="User Profile"
+                                width={30}
+                                height={30}
+                                src={"/assets/images/logo.svg"}
+                            />
+                        </button>
+                        {toggleDropdown && (
+                            <div className="mobile-menu">
+                                <Link href={"/profile"} className="btn">
+                                    My Profile
+                                </Link>
+                                <Link href={"/create"} className="btn mt-2">
+                                    Create Post
+                                </Link>
+                                <button className="btn_reversed mt-2">
+                                    Sign out
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
             </div>
         </nav>
     );
